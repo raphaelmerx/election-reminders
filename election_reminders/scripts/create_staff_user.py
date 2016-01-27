d3ca03458d76d6245ac444197ae75e2650b7a428
Raphael Merx <raphaelm@captricity.com>
2016-01-26 18:44:50 -0800

    Squash migrations

--- election_reminders/scripts/create_staff_user.py
+++ election_reminders/scripts/create_staff_user.py
@@ -9,6 +9,6 @@ from django.conf import settings
 
 if settings.DEBUG != True:
     raise Exception('Cannot create a default staff in a deployed environment')
-u, _ = User.objects.get_or_create(username='staff', is_staff=True)
+u, _ = User.objects.get_or_create(username='staff', is_staff=True, is_superuser=True)
 u.set_password('1234')
 u.save()
